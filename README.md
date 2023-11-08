a2a095672f68bc04460d186707d81fd4d4aa9887
Daniel Felix <felixdaniel9@gmail.com>
2023-11-08 11:59:31 -0300

    Refactoring javascript code and adding README file

---
+++ README.md
@@ -0,0 +1,24 @@
+# Projeto Urna Eletrônica em JavaScript
+
+<div align="center">
+<img src="https://github.com/danielfelix45/projeto-urna_eletronica-javascript/assets/81331726/5310915e-dc8a-4b32-a3a8-13de5c562f40" width="700px" />
+</div>
+
+Este é um projeto de urna eletrônica simples desenvolvido em JavaScript, HTML e CSS. A urna eletrônica é um exemplo prático de como criar uma aplicação para contagem de votos utilizando tecnologias web.
+
+## Funcionalidades
+
+- Simula uma urna eletrônica com votos para candidatos fictícios.
+- Exibe os resultados da votação em tempo real.
+- Permite a reinicialização da votação.
+
+## Pré-requisitos
+
+Certifique-se de ter um navegador web moderno instalado para executar o projeto.
+
+## Como Usar
+
+1. Clone o repositório para o seu computador:
+
+   ```bash
+   git clone https://github.com/danielfelix45/projeto-urna_eletronica-javascript.git
--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-let seuVotoPara = document.querySelector('.d-1-1 span');
-let cargo = document.querySelector('.d-1-2 span');
-let descricao = document.querySelector('.d-1-4');
-let aviso = document.querySelector('.d-2');
-let lateral = document.querySelector('.d-1-right');
-let numeros = document.querySelector('.d-1-3');
+// Seleção de elementos
+const seuVotoPara = document.querySelector('.d-1-1 span');
+const cargo = document.querySelector('.d-1-2 span');
+const descricao = document.querySelector('.d-1-4');
+const aviso = document.querySelector('.d-2');
+const lateral = document.querySelector('.d-1-right');
+const numeros = document.querySelector('.d-1-3');
 
+// Variáveis
 let etapaAtual = 0;
 let numero = '';
 let votoBranco = false;
 let votos = [];
 
+// Função para iniciar a etapa
 function comecarEtapa() {
-    let etapa = etapas[etapaAtual];
-
+    const etapa = etapas[etapaAtual];
+    
     let numeroHtml = '';
     numero = '';
     votoBranco = false;
@@ -24,36 +27,31 @@ function comecarEtapa() {
             numeroHtml += '<div class="numero"></div>';
         }
     }
-
+    
     seuVotoPara.style.display = 'none';
-    cargo.innerHTML = etapa.titulo;
+    cargo.textContent = etapa.titulo;
     descricao.innerHTML = '';
     aviso.style.display = 'none';
     lateral.innerHTML = '';
     numeros.innerHTML = numeroHtml;
 }
+
+// Função para atualizar a interface
 function atualizaInterface() {
-    let etapa = etapas[etapaAtual];
-    let candidato = etapa.candidatos.filter((item)=>{
-        if(item.numero === numero) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-    if(candidato.length > 0) {
-        candidato = candidato[0];
+    const etapa = etapas[etapaAtual];
+    const candidato = etapa.candidatos.find(item => item.numero === numero);
+
+    if (candidato) {
         seuVotoPara.style.display = 'block';
         aviso.style.display = 'block';
         descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
         let fotosHtml = '';
-        for(let i in candidato.fotos) {
-            if(candidato.fotos[i].small) {
-                fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
-            } else {
-                fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`;
-            }
+
+        for (const foto of candidato.fotos) {
+            const classe = foto.small ? 'small' : '';
+            fotosHtml += `<div class="d-1-image ${classe}"><img src="images/${foto.url}" alt="" />${foto.legenda}</div>`;
         }
+
         lateral.innerHTML = fotosHtml;
     } else {
         seuVotoPara.style.display = 'block';
@@ -62,20 +60,26 @@ function atualizaInterface() {
     }
 }
 
+// Função para tratar o clique nos números
 function clicou(n) {
-    let elNumero = document.querySelector('.numero.pisca');
-    if(elNumero !== null) {
-        elNumero.innerHTML = n;
+    const elNumero = document.querySelector('.numero.pisca');
+
+    if (elNumero) {
+        elNumero.textContent = n;
         numero = `${numero}${n}`;
 
         elNumero.classList.remove('pisca');
-        if(elNumero.nextElementSibling !== null) {
-            elNumero.nextElementSibling.classList.add('pisca');
+        const proximoNumero = elNumero.nextElementSibling;
+
+        if (proximoNumero) {
+            proximoNumero.classList.add('pisca');
         } else {
             atualizaInterface();
         }
     }
 }
+
+// Função para votar em branco
 function branco() {
     numero = '';
     votoBranco = true;
@@ -86,31 +90,29 @@ function branco() {
     descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
     lateral.innerHTML = '';
 }
+
+// Função para corrigir o voto
 function corrige() {
     comecarEtapa();
 }
-function confirma() {
-    let etapa = etapas[etapaAtual];
 
+// Função para confirmar o voto
+function confirma() {
+    const etapa = etapas[etapaAtual];
     let votoConfirmado = false;
 
-    if(votoBranco === true) {
+    if (votoBranco) {
         votoConfirmado = true;
-        votos.push({
-            etapa: etapas[etapaAtual].titulo,
-            voto: 'branco'
-        });
-    } else if(numero.length === etapa.numeros) {
+        votos.push({ etapa: etapa.titulo, voto: 'branco' });
+    } else if (numero.length === etapa.numeros) {
         votoConfirmado = true;
-        votos.push({
-            etapa: etapas[etapaAtual].titulo,
-            voto: numero
-        });
+        votos.push({ etapa: etapa.titulo, voto: numero });
     }
 
-    if(votoConfirmado) {
+    if (votoConfirmado) {
         etapaAtual++;
-        if(etapas[etapaAtual] !== undefined) {
+
+        if (etapas[etapaAtual]) {
             comecarEtapa();
         } else {
             document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
@@ -119,4 +121,5 @@ function confirma() {
     }
 }
 
+// Iniciar a primeira etapa
 comecarEtapa();
